c960e8cca2b9d83509232feb9cfef3960c280c3c
Aidos Yedilbayev <aidos.fm@gmail.com>
2018-09-03 23:55:31 +0600

    Base CRUD

--- controllers/admin/ResultController.php
+++ controllers/admin/ResultController.php
@@ -21,7 +21,7 @@ class ResultController extends Controller
     {
         return [
             'verbs' => [
-                'class'   => VerbFilter::className(),
+                'class'   => VerbFilter::class,
                 'actions' => [
                     'delete' => ['POST'],
                 ],
