094bdd7aa320d1e895460a9703def8b923741f65
Aidos Yedilbayev <aidos.fm@gmail.com>
2018-09-07 00:24:58 +0600

    CRUD for questionnaire

--- controllers/admin/QuestionnaireController.php
+++ controllers/admin/QuestionnaireController.php
@@ -2,9 +2,12 @@
 
 namespace app\controllers\admin;
 
+use app\models\Answer;
+use app\models\Question;
 use Yii;
 use app\models\Questionnaire;
 use app\models\search\QuestionnaireSearch;
+use yii\base\Model;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -54,8 +57,12 @@ class QuestionnaireController extends Controller
      */
     public function actionView($id)
     {
+        $questionnaire = $this->findModel($id);
+        $questions     = $questionnaire->getQuestions();
+
         return $this->render('view', [
-            'model' => $this->findModel($id),
+            'questionnaire' => $questionnaire,
+            'questions'     => $questions,
         ]);
     }
 
@@ -67,14 +74,76 @@ class QuestionnaireController extends Controller
      */
     public function actionCreate()
     {
-        $model = new Questionnaire();
+        $modelQuestionnaire = new Questionnaire();
+        $modelQuestions     = [new Question()];
+        $modelAnswers       = [[new Answer()]];
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+        if ($questionnaire->load(Yii::$app->request->post())) {
+
+            $questions = Model::createMultiple(Question::class);
+            Model::loadMultiple($questions, Yii::$app->request->post());
+
+            // validate person and houses models
+            $valid = $questionnaire->validate();
+            $valid = Model::validateMultiple($questions) && $valid;
+
+            if (isset($_POST['Answer'][0][0])) {
+                foreach ($_POST['Answer'] as $indexQuestion => $answers) {
+                    foreach ($answers as $indexAnswer => $answer) {
+                        $data['Answer'] = $answer;
+                        $modelAnswer = new Room;
+                        $modelRoom->load($data);
+                        $modelsRoom[$indexHouse][$indexRoom] = $modelRoom;
+                        $valid = $modelRoom->validate();
+                    }
+                }
+            }
+
+            if ($valid) {
+                $transaction = Yii::$app->db->beginTransaction();
+                try {
+                    if ($flag = $modelPerson->save(false)) {
+                        foreach ($modelsHouse as $indexHouse => $modelHouse) {
+
+                            if ($flag === false) {
+                                break;
+                            }
+
+                            $modelHouse->person_id = $modelPerson->id;
+
+                            if (!($flag = $modelHouse->save(false))) {
+                                break;
+                            }
+
+                            if (isset($modelsRoom[$indexHouse]) && is_array($modelsRoom[$indexHouse])) {
+                                foreach ($modelsRoom[$indexHouse] as $indexRoom => $modelRoom) {
+                                    $modelRoom->house_id = $modelHouse->id;
+                                    if (!($flag = $modelRoom->save(false))) {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if ($flag) {
+                        $transaction->commit();
+                        return $this->redirect(['view', 'id' => $modelPerson->id]);
+                    } else {
+                        $transaction->rollBack();
+                    }
+                } catch (Exception $e) {
+                    $transaction->rollBack();
+                }
+            }
         }
 
+
+
+
+
         return $this->render('create', [
-            'model' => $model,
+            'model' => $questionnaire,
         ]);
     }
 
--- views/admin/questionnaire/_form.php
+++ views/admin/questionnaire/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use wbraganca\dynamicform\DynamicFormWidget;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Questionnaire */
@@ -10,11 +11,70 @@ use yii\widgets\ActiveForm;
 
 <div class="questionnaire-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <div class="padding-v-md">
+        <div class="line line-dashed"></div>
+    </div>
+
+    <?php DynamicFormWidget::begin([
+        'widgetContainer' => 'dynamicform_wrapper',
+        'widgetBody' => '.container-items',
+        'widgetItem' => '.house-item',
+        'limit' => 10,
+        'min' => 1,
+        'insertButton' => '.add-house',
+        'deleteButton' => '.remove-house',
+        'model' => $modelsHouse[0],
+        'formId' => 'dynamic-form',
+        'formFields' => [
+            'description',
+        ],
+    ]); ?>
+
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>Вопросы</th>
+            <th style="width: 450px;">Ответы</th>
+            <th class="text-center" style="width: 90px;">
+                <button type="button" class="add-house btn btn-success btn-xs"><span class="fa fa-plus"></span></button>
+            </th>
+        </tr>
+        </thead>
+        <tbody class="container-items">
+        <?php foreach ($questions as $key => $question): ?>
+            <tr class="house-item">
+                <td class="vcenter">
+                    <?php
+                    // necessary for update action.
+                    if (!$question->isNewRecord) {
+                        echo Html::activeHiddenInput($question, "[{$key}]id");
+                    }
+                    ?>
+                    <?= $form->field($question, "[{$key}]description")->label(false)->textInput(['maxlength' => true]) ?>
+                </td>
+                <td>
+                    <?= $this->render('_form-rooms', [
+                        'form' => $form,
+                        'indexHouse' => $key,
+                        'modelsRoom' => $modelsRoom[$indexHouse],
+                    ]) ?>
+                </td>
+                <td class="text-center vcenter" style="width: 90px; verti">
+                    <button type="button" class="remove-house btn btn-danger btn-xs"><span class="fa fa-minus"></span></button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <?php DynamicFormWidget::end(); ?>
+
 
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
