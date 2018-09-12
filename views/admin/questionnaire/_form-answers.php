b0ee27ae07fcb024e0b064f4864e59a2f9565020
Aidos Yedilbayev <aidos.fm@gmail.com>
2018-09-13 00:42:26 +0600

    views for questionnaires

---
+++ views/admin/questionnaire/_form-answers.php
@@ -0,0 +1,60 @@
+<?php
+
+use wbraganca\dynamicform\DynamicFormWidget;
+use yii\helpers\Html;
+
+/**
+ * @var yii\web\View           $this
+ * @var yii\widgets\ActiveForm $form
+ * @var array                  $modelsAnswer
+ * @var \app\models\Answer     $modelAnswer
+ * @var integer                $indexQuestion
+ * @var integer                $indexAnswer
+ */
+?>
+
+<?php DynamicFormWidget::begin([
+    'widgetContainer' => 'dynamicform_inner',
+    'widgetBody'      => '.container-answers',
+    'widgetItem'      => '.answer-item',
+    'limit'           => 6,
+    'min'             => 1,
+    'insertButton'    => '.add-answer',
+    'deleteButton'    => '.remove-answer',
+    'model'           => $modelsAnswer[0],
+    'formId'          => 'dynamic-form',
+    'formFields'      => ['title'],
+]); ?>
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Ответ</th>
+            <th class="text-center">
+                <button type="button" class="add-answer btn btn-success btn-xs">
+                    <span class="glyphicon glyphicon-plus"></span>
+                </button>
+            </th>
+        </tr>
+        </thead>
+        <tbody class="container-answers">
+        <?php foreach ($modelsAnswer as $indexAnswer => $modelAnswer): ?>
+            <tr class="answer-item">
+                <td class="vcenter">
+                    <?php
+                    // necessary for update action.
+                    if (!$modelAnswer->isNewRecord) {
+                        echo Html::activeHiddenInput($modelAnswer, "[{$indexQuestion}][{$indexAnswer}]id");
+                    }
+                    ?>
+                    <?= $form->field($modelAnswer, "[{$indexQuestion}][{$indexAnswer}]title")->label(false)->textInput(['maxlength' => true]) ?>
+                </td>
+                <td class="text-center vcenter" style="width: 90px;">
+                    <button type="button" class="remove-answer btn btn-danger btn-xs">
+                        <span class="glyphicon glyphicon-minus"></span>
+                    </button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php DynamicFormWidget::end(); ?>
--- views/admin/questionnaire/_form.php
+++ views/admin/questionnaire/_form.php
@@ -1,21 +1,33 @@
 <?php
 
+use wbraganca\dynamicform\DynamicFormWidget;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use wbraganca\dynamicform\DynamicFormWidget;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\Questionnaire */
-/* @var $form yii\widgets\ActiveForm */
+/**
+ * @var yii\web\View              $this
+ * @var app\models\Questionnaire  $modelQuestionnaire
+ * @var yii\widgets\ActiveForm    $form
+ * @var \app\models\Questionnaire $modelQuestionnaire
+ * @var array                     $modelsQuestion
+ * @var integer                   $indexQuestion
+ * @var \app\models\Question      $modelQuestion
+ * @var array                     $modelsAnswer
+ */
 ?>
 
 <div class="questionnaire-form">
 
     <?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    <div class="row">
+        <div class="col-sm-6">
+            <?= $form->field($modelQuestionnaire, 'title')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-sm-6">
+            <?= $form->field($modelQuestionnaire, 'description')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
     <div class="padding-v-md">
         <div class="line line-dashed"></div>
@@ -23,61 +35,59 @@ use wbraganca\dynamicform\DynamicFormWidget;
 
     <?php DynamicFormWidget::begin([
         'widgetContainer' => 'dynamicform_wrapper',
-        'widgetBody' => '.container-items',
-        'widgetItem' => '.house-item',
-        'limit' => 10,
-        'min' => 1,
-        'insertButton' => '.add-house',
-        'deleteButton' => '.remove-house',
-        'model' => $modelsHouse[0],
-        'formId' => 'dynamic-form',
-        'formFields' => [
-            'description',
-        ],
+        'widgetBody'      => '.container-items',
+        'widgetItem'      => '.question-item',
+        'limit'           => 20,
+        'min'             => 1,
+        'insertButton'    => '.add-question',
+        'deleteButton'    => '.remove-question',
+        'model'           => $modelsQuestion[0],
+        'formId'          => 'dynamic-form',
+        'formFields'      => ['title'],
     ]); ?>
-
     <table class="table table-bordered table-striped">
         <thead>
         <tr>
-            <th>Вопросы</th>
-            <th style="width: 450px;">Ответы</th>
+            <th>questions</th>
+            <th style="width: 450px;">answers</th>
             <th class="text-center" style="width: 90px;">
-                <button type="button" class="add-house btn btn-success btn-xs"><span class="fa fa-plus"></span></button>
+                <button type="button" class="add-question btn btn-success btn-xs"><span class="fa fa-plus"></span>
+                </button>
             </th>
         </tr>
         </thead>
         <tbody class="container-items">
-        <?php foreach ($questions as $key => $question): ?>
-            <tr class="house-item">
+        <?php foreach ($modelsQuestion as $indexQuestion => $modelQuestion): ?>
+            <tr class="question-item">
                 <td class="vcenter">
                     <?php
                     // necessary for update action.
-                    if (!$question->isNewRecord) {
-                        echo Html::activeHiddenInput($question, "[{$key}]id");
+                    if (!$modelQuestion->isNewRecord) {
+                        echo Html::activeHiddenInput($modelQuestion, "[{$indexQuestion}]id");
                     }
                     ?>
-                    <?= $form->field($question, "[{$key}]description")->label(false)->textInput(['maxlength' => true]) ?>
+                    <?= $form->field($modelQuestion, "[{$indexQuestion}]title")->label(false)->textInput(['maxlength' => true]) ?>
                 </td>
                 <td>
-                    <?= $this->render('_form-rooms', [
-                        'form' => $form,
-                        'indexHouse' => $key,
-                        'modelsRoom' => $modelsRoom[$indexHouse],
+                    <?= $this->render('_form-answers', [
+                        'form'          => $form,
+                        'indexQuestion' => $indexQuestion,
+                        'modelsAnswer'  => $modelsAnswer[$indexQuestion],
                     ]) ?>
                 </td>
                 <td class="text-center vcenter" style="width: 90px; verti">
-                    <button type="button" class="remove-house btn btn-danger btn-xs"><span class="fa fa-minus"></span></button>
+                    <button type="button" class="remove-question btn btn-danger btn-xs">
+                        <span class="fa fa-minus"></span>
+                    </button>
                 </td>
             </tr>
         <?php endforeach; ?>
         </tbody>
     </table>
-
     <?php DynamicFormWidget::end(); ?>
 
-
     <div class="form-group">
-        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton($modelQuestionnaire->isNewRecord ? 'Добавить' : 'Обновить', ['class' => 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
